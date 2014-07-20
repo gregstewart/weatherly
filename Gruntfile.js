1b9d8a79cc7f1327602279989c08826a9a633c9c
Greg Stewart <gregs@tcias.co.uk>
2014-07-20 17:17:32 +0100

    Added karma and grunt task to run karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,39 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    express: {
-      test: {
-        options: {
-          script: './server.js'
-        }
-      }
-    },
-    cucumberjs: {
-      src: 'tests/e2e/features/',
-      options: {
-        steps: 'tests/e2e/steps/'
-      }
-    }
-  });
+    module.exports = function (grunt) {
+        grunt.initConfig({
+            express: {
+                test: {
+                    options: {
+                        script: './server.js'
+                    }
+                }
+            },
+            cucumberjs: {
+                src: 'tests/e2e/features/',
+                options: {
+                    steps: 'tests/e2e/steps/'
+                }
+            },
+            karma: {
+                unit: {
+                    configFile: 'karma.conf.js'
+                }
+            }
+        });
 
-  grunt.loadNpmTasks('grunt-express-server');
-  grunt.loadNpmTasks('grunt-selenium-webdriver');
-  grunt.loadNpmTasks('grunt-cucumber');
+        grunt.loadNpmTasks('grunt-express-server');
+        grunt.loadNpmTasks('grunt-selenium-webdriver');
+        grunt.loadNpmTasks('grunt-cucumber');
+        grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('e2e', [
-    'selenium_start',
-    'express:test',
-    'cucumberjs',
-    'selenium_stop',
-    'express:test:stop'
-  ]);
-};
+        grunt.registerTask('e2e', [
+            'selenium_start',
+            'express:test',
+            'cucumberjs',
+            'selenium_stop',
+            'express:test:stop'
+        ]);
+
+        grunt.registerTask('unit', [
+            'karma:unit'
+        ]);
+    };
