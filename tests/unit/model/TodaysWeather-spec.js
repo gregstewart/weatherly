ce4d35182b5e034e9e7529e7c6d99b570ab87975
Greg Stewart <gregs@tcias.co.uk>
2015-01-19 16:16:41 +0000

    Fixed linting issues

--- tests/unit/model/TodaysWeather-spec.js
+++ tests/unit/model/TodaysWeather-spec.js
@@ -1,184 +1,184 @@
-    'use strict';
+'use strict';
 
-    var TodaysWeather = require('weatherly/js/model/TodaysWeather');
+var TodaysWeather = require('weatherly/js/model/TodaysWeather');
 
-    describe('Today \'s weather', function () {
+describe('Today \'s weather', function () {
 
-        beforeEach(function () {
-            this.location = 'Boston';
-            this.country = 'US';
-            this.temperature = 47.39;
-            this.apparentTemperature = 41.78;
+    beforeEach(function () {
+        this.location = 'Boston';
+        this.country = 'US';
+        this.temperature = 47.39;
+        this.apparentTemperature = 41.78;
+    });
+
+    it('stores the values passed in', function () {
+        var todaysWeather = new TodaysWeather({ location: this.location,
+            country: this.country,
+            temperature: this.temperature,
+            apparentTemperature: this.apparentTemperature});
+
+        expect(todaysWeather.get('location')).toBe(this.location);
+        expect(todaysWeather.get('temperature')).toBe(this.temperature);
+        expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+    });
+
+    describe('Temperature conversion', function () {
+        describe('given we are in the "UK"', function () {
+            it('converts to Celsius', function () {
+                var expectedTemperature = (this.temperature - 32) * (5/9),
+                    expectedApparentTemperature = (this.apparentTemperature - 32) * (5/9),
+                    todaysWeather = new TodaysWeather({ location: 'London',
+                    country: 'UK',
+                    temperature: this.temperature,
+                    apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(expectedTemperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(expectedApparentTemperature);
+            });
         });
 
-        it('stores the values passed in', function () {
-            var todaysWeather = new TodaysWeather({ location: this.location,
-                country: this.country,
-                temperature: this.temperature,
-                apparentTemperature: this.apparentTemperature});
+        describe('given we are in a country that uses Fahrenheit', function () {
+            it('does not convert to Celsius when we are in Nassau', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Nassau',
+                        country: 'BS',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
 
-            expect(todaysWeather.get('location')).toBe(this.location);
-            expect(todaysWeather.get('temperature')).toBe(this.temperature);
-            expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in Boston', function () {
+                var todaysWeather = new TodaysWeather({ location: this.location,
+                        country: this.country,
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in Charlotte Amalie', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Charlotte Amalie',
+                        country: 'VI',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in Pago Pago', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Pago Pago',
+                        country: 'AS',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in Ngerulmud', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Ngerulmud',
+                        country: 'PW',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in George Town', function () {
+                var todaysWeather = new TodaysWeather({ location: 'George Town',
+                        country: 'KY',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
+
+            it('does not convert to Celsius when we are in Belmopan', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Belmopan',
+                        country: 'BZ',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
+            });
         });
 
-        describe('Temperature conversion', function () {
-            describe('given we are in the "UK"', function () {
-                it('converts to Celsius', function () {
-                    var expectedTemperature = (this.temperature - 32) * (5/9),
-                        expectedApparentTemperature = (this.apparentTemperature - 32) * (5/9),
-                        todaysWeather = new TodaysWeather({ location: 'London',
+        describe('Temperature changes afterwards', function () {
+
+            it('converts the temperature value if we are in the "UK"', function () {
+                var expected = (this.temperature - 32) * (5/9),
+                    changeExpected = (99 - 32) * (5/9),
+                    todaysWeather = new TodaysWeather({ location: 'London',
                         country: 'UK',
                         temperature: this.temperature,
                         apparentTemperature: this.apparentTemperature});
 
-                    expect(todaysWeather.get('temperature')).toBe(expectedTemperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(expectedApparentTemperature);
-                });
+                expect(todaysWeather.get('temperature')).toBe(expected);
+
+                todaysWeather.set('temperature', 99);
+                expect(todaysWeather.get('temperature')).toBe(changeExpected);
             });
 
-            describe('given we are in a country that uses Fahrenheit', function () {
-                it('does not convert to Celsius when we are in Nassau', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Nassau',
-                            country: 'BS',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
+            it('converts the apparentTemperature value if we are in the "UK"', function () {
+                var expected = (this.apparentTemperature - 32) * (5/9),
+                    changeExpected = (80 - 32) * (5/9),
+                    todaysWeather = new TodaysWeather({ location: 'London',
+                        country: 'UK',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
 
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
+                expect(todaysWeather.get('apparentTemperature')).toBe(expected);
 
-                it('does not convert to Celsius when we are in Boston', function () {
-                    var todaysWeather = new TodaysWeather({ location: this.location,
-                            country: this.country,
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
-
-                it('does not convert to Celsius when we are in Charlotte Amalie', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Charlotte Amalie',
-                            country: 'VI',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
-
-                it('does not convert to Celsius when we are in Pago Pago', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Pago Pago',
-                            country: 'AS',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
-
-                it('does not convert to Celsius when we are in Ngerulmud', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Ngerulmud',
-                            country: 'PW',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
-
-                it('does not convert to Celsius when we are in George Town', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'George Town',
-                            country: 'KY',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
-
-                it('does not convert to Celsius when we are in Belmopan', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Belmopan',
-                            country: 'BZ',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(this.apparentTemperature);
-                });
+                todaysWeather.set('apparentTemperature', 80);
+                expect(todaysWeather.get('apparentTemperature')).toBe(changeExpected);
             });
 
-            describe('Temperature changes afterwards', function () {
-
-                it('converts the temperature value if we are in the "UK"', function () {
-                    var expected = (this.temperature - 32) * (5/9),
-                        changeExpected = (99 - 32) * (5/9),
-                        todaysWeather = new TodaysWeather({ location: 'London',
-                            country: 'UK',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(expected);
-
-                    todaysWeather.set('temperature', 99);
-                    expect(todaysWeather.get('temperature')).toBe(changeExpected);
-                });
-
-                it('converts the apparentTemperature value if we are in the "UK"', function () {
-                    var expected = (this.apparentTemperature - 32) * (5/9),
-                        changeExpected = (80 - 32) * (5/9),
-                        todaysWeather = new TodaysWeather({ location: 'London',
-                            country: 'UK',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('apparentTemperature')).toBe(expected);
-
-                    todaysWeather.set('apparentTemperature', 80);
-                    expect(todaysWeather.get('apparentTemperature')).toBe(changeExpected);
-                });
-
-                it('does not convert the temperature value if we are in the US', function () {
-                    var todaysWeather = new TodaysWeather({ location: 'Boston',
-                            country: 'US',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
-
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
-
-                    todaysWeather.set('temperature', 80);
-                    expect(todaysWeather.get('temperature')).toBe(80);
-                });
-            });
-
-            xdescribe('Location changes', function () {
-                it('should convert if we were in a Fahrenheit country but are now in the "UK"', function () {
-                    var expected = (this.temperature - 32) * (5/9),
-                        todaysWeather = new TodaysWeather({ location: 'Boston',
+            it('does not convert the temperature value if we are in the US', function () {
+                var todaysWeather = new TodaysWeather({ location: 'Boston',
                         country: 'US',
                         temperature: this.temperature,
                         apparentTemperature: this.apparentTemperature});
 
-                    expect(todaysWeather.get('temperature')).toBe(this.temperature);
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
 
-                    todaysWeather.set('location', 'UK');
-                    expect(todaysWeather.get('temperature')).toBe(expected);
-                });
+                todaysWeather.set('temperature', 80);
+                expect(todaysWeather.get('temperature')).toBe(80);
+            });
+        });
 
-                it('should convert if we were in a Celsius country but are now in the "US"', function () {
-                    var originalTemperature = this.temperature,
-                        expected = (this.temperature - 32) * (5/9),
-                        todaysWeather = new TodaysWeather({ location: 'London',
-                            country: 'UK',
-                            temperature: this.temperature,
-                            apparentTemperature: this.apparentTemperature});
+        xdescribe('Location changes', function () {
+            it('should convert if we were in a Fahrenheit country but are now in the "UK"', function () {
+                var expected = (this.temperature - 32) * (5/9),
+                    todaysWeather = new TodaysWeather({ location: 'Boston',
+                    country: 'US',
+                    temperature: this.temperature,
+                    apparentTemperature: this.apparentTemperature});
 
-                    expect(todaysWeather.get('temperature')).toBe(expected);
+                expect(todaysWeather.get('temperature')).toBe(this.temperature);
 
-                    todaysWeather.set('location', 'US');
-                    expect(todaysWeather.get('temperature')).toBe(originalTemperature);
-                });
+                todaysWeather.set('location', 'UK');
+                expect(todaysWeather.get('temperature')).toBe(expected);
+            });
+
+            it('should convert if we were in a Celsius country but are now in the "US"', function () {
+                var originalTemperature = this.temperature,
+                    expected = (this.temperature - 32) * (5/9),
+                    todaysWeather = new TodaysWeather({ location: 'London',
+                        country: 'UK',
+                        temperature: this.temperature,
+                        apparentTemperature: this.apparentTemperature});
+
+                expect(todaysWeather.get('temperature')).toBe(expected);
+
+                todaysWeather.set('location', 'US');
+                expect(todaysWeather.get('temperature')).toBe(originalTemperature);
             });
         });
     });
+});
